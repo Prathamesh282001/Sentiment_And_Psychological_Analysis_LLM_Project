d24c99135516ea039c85146f78e6de056f7c6b0c
Prathamesh282001 <chavan.prthm28@gmail.com>
2024-04-13 19:49:19 +0530

    first commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 from src.speech_to_text import transcribe_audio
 from src.llm import analyze_text
@@ -10,14 +11,24 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    if 'audio' not in request.files:
+        return "No audio file uploaded"
+
     audio_file = request.files['audio']
-    
-    transcribed_text = transcribe_audio(audio_file.filename)
+
+    if audio_file.filename == '':
+        return "No selected file"
+
+    temp_file_path = "/tmp/" + audio_file.filename
+    audio_file.save(temp_file_path)
+
+    transcribed_text = transcribe_audio(temp_file_path)
+
     if transcribed_text:
         result = analyze_text(transcribed_text)
         return render_template('index.html', output=result)
     else:
-        return "Error in upload function."
+        return "Error in transcription"
 
 if __name__ == '__main__':
     app.run(debug=True)
