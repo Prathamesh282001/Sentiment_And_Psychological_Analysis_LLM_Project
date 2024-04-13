e448d33b1b0e364b016f4e487b9381b4c0e21f05
Prathamesh282001 <chavan.prthm28@gmail.com>
2024-04-13 19:56:29 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""import os
+import os
 from flask import Flask, request, render_template
 from src.speech_to_text import transcribe_audio
 from src.llm import analyze_text
@@ -29,30 +29,6 @@ def upload():
         return render_template('index.html', output=result)
     else:
         return "Error in transcription"
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-from flask import Flask, request, render_template
-from src.speech_to_text import transcribe_audio
-from src.llm import analyze_text
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    audio_file = request.files['audio']
-    
-    transcribed_text = transcribe_audio(audio_file.filename)
-    if transcribed_text:
-        result = analyze_text(transcribed_text)
-        return render_template('index.html', output=result)
-    else:
-        return "Error in upload function."
 
 if __name__ == '__main__':
     app.run(debug=True)
